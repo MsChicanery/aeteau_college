31ce8875295633d99aa4d96e2bfa458d6c5724c5
KKbutter2281 <kalebarora@outlook.com>
2024-12-15 14:24:50 -0500

    Update page.tsx

--- src/app/faculty/page.tsx
+++ src/app/faculty/page.tsx
@@ -20,7 +20,7 @@ const faculty = [
     name: "Dr. Butter A. Osmanli",
     title: "Dean of Osmanli School Of Law",
     description: "Butter Osmanli is a troll from under the bridge. You can see him posting memes all over A2C in the worst ways possible.",
-    image: "../public/faculty/butter.png"
+    image: "faculty/butter.png"
   },
   {
     name: "Dr. Carly T",
