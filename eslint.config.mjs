5f0d5e58c562ef00240f47eb98cb1e2deb25accf
KKbutter2281 <kalebarora@outlook.com>
2024-12-16 18:00:44 -0500

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,26 @@
-import { FlatCompat } from '@eslint/eslintrc'
+import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
-})
+});
 
 const eslintConfig = [
   ...compat.config({
     extends: ['next'],
     rules: {
+      // Disabling all rules
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/jsx-uses-vars': 'off',
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+      'no-console': 'off',
+      'no-extra-semi': 'off',
+      // Add any other rules you want to disable here
     },
   }),
-]
+];
 
-export default eslintConfig
+export default eslintConfig;
