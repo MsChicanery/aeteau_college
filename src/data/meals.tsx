f93f585c10c9479d76c0c0ca0f534b05d4b2e2a5
Kaleb A <kalebarora@outlook.com>
2025-01-02 09:25:46 -0500

    Create meals.tsx

---
+++ src/data/meals.tsx
@@ -0,0 +1,7 @@
+const meals = {
+  breakfast: ["Pancakes", "Scrambled Eggs", "Omelette", "French Toast", "Cereal", "Fruit Salad"],
+  lunch: ["Grilled Chicken Sandwich", "Caesar Salad", "Spaghetti Bolognese", "Beef Stir-fry", "Turkey Club Sandwich", "Tomato Soup"],
+  dinner: ["Steak", "Salmon", "Vegetable Stir-fry", "Chicken Alfredo", "Tacos", "Pizza"],
+};
+
+export default meals;
